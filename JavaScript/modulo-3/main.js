bae25f4df5fa1eb922501d0f19a54234827751b6
Caio Cichetti <caio_cichetti@outlook.com>
2020-05-03 21:25:00 -0300

    requisições AJAX

---
+++ JavaScript/modulo-3/main.js
@@ -0,0 +1,11 @@
+var xhr = new XMLHttpRequest();
+
+// buscando dados da api do github
+xhr.open('GET', 'https://api.github.com/users/caiocichetti')
+xhr.send(null);
+
+xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4) {
+        console.log(JSON.parse(xhr.responseText));
+    }
+}
