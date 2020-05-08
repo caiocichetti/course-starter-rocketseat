ced8c2a9d938baf94d73ac3b1be9e243eb79a03d
Caio Cichetti <caio_cichetti@outlook.com>
2020-05-08 10:37:44 -0300

    Update bundle.js

--- javascript-es6/bundle.js
+++ javascript-es6/bundle.js
@@ -1,6 +1,8 @@
 "use strict";
 
-function _readOnlyError(name) { throw new Error("\"" + name + "\" is read-only"); }
-
-var a = 1;
-a = (_readOnlyError("a"), 3);
+// Mutação
+var usuario = {
+  nome: 'Caio'
+};
+usuario.nome = 'Diego';
+console.log(usuario);
