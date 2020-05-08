fe75bb94382d3011bd4fe64e2fb82d428f739a48
Caio Cichetti <caio_cichetti@outlook.com>
2020-05-08 10:29:13 -0300

    Update main.js
    Utilizando static

--- javascript-es6/main.js
+++ javascript-es6/main.js
@@ -1,33 +1,7 @@
-class List {
-    // primeiro elemento a ser executado quando um objeto for instanciado
-    constructor() {
-        this.data = [];
-    }
-
-    add(data) {
-        this.data.push(data);
-        console.log(this.data);
+class Matematica {
+    static soma(a, b) {
+        return a + b;
     }
 }
 
-// utilizando herança
-class TodoList extends List {
-    constructor() {
-        //chamando o constructor da classe pai
-        super();
-
-        this.usuario = 'Caio';
-    }
-
-    mostraUsuario() {
-        console.log(this.usuario);
-    }
-}
-
-var MinhaLista = new TodoList();
-
-document.getElementById('novotodo').onclick = function() {
-    MinhaLista.add('Novo todo');
-}
-
-MinhaLista.mostraUsuario();
+console.log(Matematica.soma(1, 2));
