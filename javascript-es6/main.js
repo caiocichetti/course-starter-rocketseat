12f843848c059d9fa295912909945a278f20ad76
Caio Cichetti <caio_cichetti@outlook.com>
2020-05-08 10:22:37 -0300

    Update main.js

--- javascript-es6/main.js
+++ javascript-es6/main.js
@@ -1,16 +1,33 @@
-class TodoList {
+class List {
+    // primeiro elemento a ser executado quando um objeto for instanciado
     constructor() {
-        this.todos = [];
+        this.data = [];
     }
 
-    addTodo() {
-        this.todos.push('Novo todo');
-        console.log(this.todos);
+    add(data) {
+        this.data.push(data);
+        console.log(this.data);
     }
 }
 
-const MinhaLista = new TodoList();
+// utilizando herança
+class TodoList extends List {
+    constructor() {
+        //chamando o constructor da classe pai
+        super();
+
+        this.usuario = 'Caio';
+    }
+
+    mostraUsuario() {
+        console.log(this.usuario);
+    }
+}
+
+var MinhaLista = new TodoList();
 
 document.getElementById('novotodo').onclick = function() {
-    MinhaLista.addTodo();
+    MinhaLista.add('Novo todo');
 }
+
+MinhaLista.mostraUsuario();
