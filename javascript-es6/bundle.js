7f7a8dfc8b80698098eea104720b2b6f4ea12473
Caio Cichetti <caio_cichetti@outlook.com>
2020-05-10 22:32:00 -0300

    Operações em Array

--- javascript-es6/bundle.js
+++ javascript-es6/bundle.js
@@ -1,13 +1,27 @@
 "use strict";
 
-function teste(x) {
-  // variáveis de escopo
-  var y = 2;
+var arr = [1, 3, 4, 5, 8, 9];
+var newArr = arr.map(function (item, index) {
+  return item + index;
+});
+console.log(newArr); // [1, 4, 6, 8, 12, 14]
 
-  if (x > 5) {
-    var _y = 4;
-    console.log(x, _y);
-  }
-}
+var sum = arr.reduce(function (total, next) {
+  return total + next; // 0
+  // 1
+  // 1
+  // 3
+  // 4
+  // 4
+});
+console.log(sum); // 30
 
-teste(10);
+var filter = arr.filter(function (item) {
+  return item % 2 === 0;
+});
+console.log(filter); // [4, 8]
+
+var find = arr.find(function (item) {
+  return item === 4;
+});
+console.log(find); // 4
--- javascript-es6/main.js
+++ javascript-es6/main.js
@@ -1,12 +1,33 @@
-function teste(x) {
-    // variáveis de escopo
-    let y = 2;
+const arr = [1, 3, 4, 5, 8, 9];
 
-    if (x > 5) {
-        let y = 4;
+const newArr = arr.map(function(item, index) {
+    return item + index;
+});
 
-        console.log(x, y);
-    }
-}
+console.log(newArr); // [1, 4, 6, 8, 12, 14]
 
-teste(10);
+const sum = arr.reduce(function(total, next) {
+    return total + next; 
+    // 0
+    // 1
+
+    // 1
+    // 3
+
+    // 4
+    // 4
+});
+
+console.log(sum); // 30
+
+const filter = arr.filter(function(item) {
+    return item % 2 === 0;
+});
+
+console.log(filter) // [4, 8]
+
+const find = arr.find(function(item) {
+    return item === 4;
+})
+
+console.log(find); // 4
