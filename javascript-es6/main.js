d3397da0932ea95a5863309c92184fbeb3940a5a
Caio Cichetti <caio_cichetti@outlook.com>
2020-05-16 16:51:30 -0300

    Operadores rest/spread

--- javascript-es6/main.js
+++ javascript-es6/main.js
@@ -1,15 +1,47 @@
+// REST - pega o resto das propriedades
+
+// exemplos de utilidade
 const usuario = {
     nome: 'Caio',
     idade: 17,
-    endereco: {
-        cidade: 'Campinas',
-        estado: 'SP'
-    },
+    empresa: 'Estudante'
 };
 
-// Desestruturando o objeto
-function mostraNome( {nome, idade} ) {
-    console.log(nome, idade);
+const { nome, ...resto } = usuario;
+
+console.log(nome);
+console.log(resto);
+
+const arr = [1, 2, 3, 4];
+
+const [ a, b, ...c] = arr;
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+function soma(a, b,...params) {
+    return params.reduce((total, next) => total + next);
 }
 
-mostraNome(usuario);
+console.log(soma(1, 3, 4));
+
+// SPREAD - propaga valores
+
+// exemplos de utilidade
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+
+const arr3 = [ ...arr1, ...arr2 ];
+
+console.log(arr3);
+
+const usuario1 = {
+    nome: 'Caio',
+    idade: 17,
+    empresa: 'Estudante'
+}
+
+const usuario2 = { ...usuario1, nome:"Diego"};
+
+console.log(usuario2);
