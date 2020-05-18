44490bfb1081044657e0f8fc363edc6d94736349
Caio Cichetti <caio_cichetti@outlook.com>
2020-05-17 22:12:14 -0300

    Import/export part1

--- javascript-es6/main.js
+++ javascript-es6/main.js
@@ -1,3 +1,5 @@
-import { soma } from './funcoes';
+import soma, {  sub  as subtraiFunction,  } from './funcoes';
+// import somaFunction from './soma';
 
 console.log(soma(1, 2));
+console.log(subtraiFunction(4, 2));
