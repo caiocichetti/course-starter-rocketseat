e7af7d405fde1a1895a4b3c784de61a426b6e433
Caio Cichetti <caio_cichetti@outlook.com>
2020-05-19 00:18:28 -0300

    Loading e error | aplicação finalizada 🚀

--- javascript-es6/src/main.js
+++ javascript-es6/src/main.js
@@ -14,6 +14,18 @@ class App {
 		this.formEl.onsubmit = event => this.addRepository(event);
 	}
 
+	setLoading(loading = true) {
+		if (loading === true) {
+			let loadingEl = document.createElement('span');
+			loadingEl.appendChild(document.createTextNode('Carregando'));
+			loadingEl.setAttribute('id', 'loading');
+
+			this.formEl.appendChild(loadingEl);
+		} else {
+			document.getElementById('loading').remove();
+		}
+	}
+
 	async addRepository(event) {
 		event.preventDefault();
 
@@ -22,20 +34,28 @@ class App {
 		if(repoInput.length === 0) 
 			return;
 		
-		const response = await api.get(`/repos/${repoInput}`);
-
-		const { name, description, html_url, owner: { avatar_url } } = response.data
-
-		this.repositories.push({
-			name,
-			description,
-			avatar_url,
-			html_url,
-		});
-
-		this.inputEl.value = '';
+		this.setLoading();
 		
-		this.render()
+		try {
+			const response = await api.get(`/repos/${repoInput}`);
+	
+			const { name, description, html_url, owner: { avatar_url } } = response.data
+	
+			this.repositories.push({
+				name,
+				description,
+				avatar_url,
+				html_url,
+			});
+	
+			this.inputEl.value = '';
+	
+			this.render()
+		} catch (err) {
+			alert('O repositório não existe!');
+			this.inputEl.value = '';
+		}
+		this.setLoading(false);
 	}
 
 	render() {
