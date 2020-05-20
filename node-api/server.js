bfac5f24d7c2e380afd7e8cdc82fb5e9c93f3403
Caio Cichetti <caio_cichetti@outlook.com>
2020-05-19 23:28:38 -0300

    Conectando Database

--- node-api/server.js
+++ node-api/server.js
@@ -1,9 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
+// Iniciando o App
 const app = express();
 
+// Iniciando o DB
+mongoose.connect("mongodb://localhost:27017/nodeapi", {
+  useNewUrlParser: true,
+});
+
+// Primeira rota
 app.get("/", (req, res) => {
-    res.send("Hello Rocketseat");
+  res.send("Hello Rocketseat");
 });
 
 app.listen(3001);
