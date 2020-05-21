a8c2d0d50324a16e3047b7de5574432ab220aefe
Caio Cichetti <caio_cichetti@outlook.com>
2020-05-21 16:29:46 -0300

    CRUD
    Create, Read, Update and Delete.

--- node-api/src/controllers/ProductController.js
+++ node-api/src/controllers/ProductController.js
@@ -4,14 +4,39 @@ const Product = mongoose.model("Product");
 
 module.exports = {
   async index(req, res) {
+    // Listagem
     const products = await Product.find();
 
     return res.json(products);
   },
+
+  async show(req, res) {
+    // Detalhe
+    const product = await Product.findById(req.params.id);
+
+    return res.json(product);
+  },
+
   async store(req, res) {
-    // Criação
+    // Criar
     const product = await Product.create(req.body);
 
     return res.json(product);
+  },
+
+  async update(req, res) {
+    // Alterar
+    const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+
+    return res.json(product);
+  },
+
+  async destroy(req, res) {
+    // Apagar
+    await Product.findByIdAndRemove(req.params.id);
+
+    return res.send();
   },
 };
