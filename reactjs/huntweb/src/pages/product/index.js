bf664465f802a7e48aab87f0de3ba5afb92417d7
Caio Cichetti <caio_cichetti@outlook.com>
2020-07-18 11:13:40 -0300

    botão voltar para /

--- reactjs/huntweb/src/pages/product/index.js
+++ reactjs/huntweb/src/pages/product/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import api from '../../services/api';
+import { Link } from "react-router-dom";
 
 import './styles.css';
 
@@ -20,13 +21,18 @@ export default class Product extends Component {
     const { product } = this.state;
 
     return (
-      <div className="product-info">
-        <h1>{product.title}</h1>
-        <h2>{product.description}</h2>
-        <p>
-           URL: <a href={product.url}>{product.url}</a>
-        </p>
-      </div>
+      <>
+        <div className="product-info">
+          <h1>{product.title}</h1>
+          <h2>{product.description}</h2>
+          <p>
+            URL: <a href={product.url}>{product.url}</a>
+          </p>
+          <div className="option">
+            <Link className="button-back" to="/">Voltar</Link>
+          </div>
+        </div>
+      </>
     );
   }
 }
